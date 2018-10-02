2551729495f8b26fdda5a33da0fcadf90a5b208b
Petr Karmashev <smonkl@bk.ru>
2018-10-02 18:44:12 +0300

    timers sample

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
 	fmt.Println("Welcome to your shop! Please sit back and wait for clients.")
 }
-
